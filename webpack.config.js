8b4118ec11005e3c55c05f5576025f284337d7e9
Jovert Palonpon <j.palonpon@iparentportal.com>
2018-12-01 08:51:54 +0800

    build step - minor config update

--- webpack.config.js
+++ webpack.config.js
@@ -1,126 +1,126 @@
 /**
  * * Generic
  */
-const path = require("path");
-const fs = require("fs");
-const notifier = require("node-notifier");
+const path = require('path');
+const fs = require('fs');
+const notifier = require('node-notifier');
 
 /**
  * * Plugins.
  */
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-const CleanObsoleteChunks = require("webpack-clean-obsolete-chunks");
-const EventHooksPlugin = require("event-hooks-webpack-plugin");
-const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CleanObsoleteChunks = require('webpack-clean-obsolete-chunks');
+const EventHooksPlugin = require('event-hooks-webpack-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 /**
  * * Globals
  */
-const inProduction = process.env.NODE_ENV === "production";
-const PUBLIC_DIR = path.resolve(__dirname, "./public");
-const SRC_DIR = path.resolve(__dirname, "./resources/assets");
-const DIST_DIR = path.resolve(__dirname, "./public");
+const inProduction = process.env.NODE_ENV === 'production';
+const PUBLIC_DIR = path.resolve(__dirname, './public');
+const SRC_DIR = path.resolve(__dirname, './resources/assets');
+const DIST_DIR = path.resolve(__dirname, './public');
 
 module.exports = {
     mode: process.env.NODE_ENV,
 
     entry: {
-        "__backend/app": path.join(SRC_DIR, "__backend/index.js"),
-        "__backend/vendor": [
-            "moment",
-            "lodash",
-            "axios",
-            "react",
-            "react-dom",
-            "react-router-dom",
-            "react-md",
-            "react-md/dist/react-md.blue-deep_orange.min.css"
+        '__backend/app': path.join(SRC_DIR, '__backend/index.js'),
+        '__backend/vendor': [
+            'moment',
+            'lodash',
+            'axios',
+            'react',
+            'react-dom',
+            'react-router-dom',
+            'react-md',
+            'react-md/dist/react-md.blue-deep_orange.min.css',
         ],
 
-        "__frontend/app": path.join(SRC_DIR, "__frontend/index.js"),
-        "__frontend/vendor": [
-            "moment",
-            "lodash",
-            "axios",
-            "react",
-            "react-dom",
-            "react-router-dom"
-        ]
+        '__frontend/app': path.join(SRC_DIR, '__frontend/index.js'),
+        '__frontend/vendor': [
+            'moment',
+            'lodash',
+            'axios',
+            'react',
+            'react-dom',
+            'react-router-dom',
+        ],
     },
 
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader"]
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
             },
 
             {
                 test: /\.s(a|c)ss$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
             },
 
             {
                 test: /\.(js|jsx)$/,
-                exclude: "/node_modules/",
-                use: "babel-loader"
+                exclude: '/node_modules/',
+                use: 'babel-loader',
             },
 
             {
                 test: /\.(eot|ttf|woff|woff2)$/,
                 use: [
                     {
-                        loader: "file-loader",
+                        loader: 'file-loader',
                         options: {
-                            name: "fonts/[name].[hash].[ext]"
-                        }
-                    }
-                ]
+                            name: 'fonts/[name].[hash].[ext]',
+                        },
+                    },
+                ],
             },
 
             {
                 test: /\.(jpe?g|png|gif|svg)$/i,
                 use: [
                     {
-                        loader: "url-loader",
+                        loader: 'url-loader',
                         options: {
-                            name: "/img/[name].[hash].[ext]",
-                            fallback: "file-loader"
-                        }
+                            name: '/img/[name].[hash].[ext]',
+                            fallback: 'file-loader',
+                        },
                     },
 
                     {
-                        loader: "img-loader",
+                        loader: 'img-loader',
                         options: {
                             plugins: [
-                                require("imagemin-mozjpeg")(),
-                                require("imagemin-pngquant")(),
-                                require("imagemin-gifsicle")(),
-                                require("imagemin-svgo")()
-                            ]
-                        }
-                    }
-                ]
-            }
-        ]
+                                require('imagemin-mozjpeg')(),
+                                require('imagemin-pngquant')(),
+                                require('imagemin-gifsicle')(),
+                                require('imagemin-svgo')(),
+                            ],
+                        },
+                    },
+                ],
+            },
+        ],
     },
 
     plugins: [
         new MiniCssExtractPlugin({
-            filename: "css/[name].bundle.[contenthash].css"
+            filename: 'css/[name].bundle.[contenthash].css',
         }),
 
-        new CleanWebpackPlugin(["css", "js", "img", "fonts"], {
+        new CleanWebpackPlugin(['css', 'js', 'img', 'fonts'], {
             root: DIST_DIR,
             exclude: [],
             verbose: true,
-            dry: false
+            dry: false,
         }),
 
         new CleanObsoleteChunks({
             verbose: true,
-            deep: true
+            deep: true,
         }),
 
         new EventHooksPlugin({
@@ -130,49 +130,49 @@ module.exports = {
 
                 notifier.notify({
                     title:
-                        errors.length > 0 ? "Build Failed" : "Build Successful",
+                        errors.length > 0 ? 'Build Failed' : 'Build Successful',
                     message: `Completed in ${time}ms`,
                     icon: path.resolve(
                         __dirname,
-                        "./public/img/logo-white.png"
+                        './public/img/logo-white.png',
                     ),
                     sound: true,
-                    wait: true
+                    wait: true,
                 });
 
                 let manifest = {};
 
                 stats.toJson().assets.forEach(({ name }) => {
-                    let ext = name.split(".").reverse()[0];
-                    let key = `${name.substring(0, name.indexOf("."))}.${ext}`;
+                    let ext = name.split('.').reverse()[0];
+                    let key = `${name.substring(0, name.indexOf('.'))}.${ext}`;
 
                     Object.assign(manifest, {
-                        [key]: name
+                        [key]: name,
                     });
                 });
 
                 fs.writeFileSync(
-                    path.resolve(DIST_DIR, "./manifest.json"),
-                    JSON.stringify(manifest, null, 2)
+                    path.resolve(DIST_DIR, './manifest.json'),
+                    JSON.stringify(manifest, null, 2),
                 );
-            }
-        })
+            },
+        }),
     ],
 
     output: {
         path: DIST_DIR,
         publicPath: PUBLIC_DIR,
-        filename: "js/[name].bundle.[contenthash].js"
+        filename: 'js/[name].bundle.[contenthash].js',
     },
 
     optimization: {
-        minimize: inProduction
+        minimize: inProduction,
     },
 
     node: {
         __filename: true,
-        __dirname: true
-    }
+        __dirname: true,
+    },
 };
 
 if (!inProduction) {
@@ -181,7 +181,7 @@ if (!inProduction) {
             host: 'localhost',
             proxy: 'localhost:8000',
             open: true,
-            notify: false
-        })
+            notify: false,
+        }),
     );
 }
